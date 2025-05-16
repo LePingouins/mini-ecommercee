bc5a50c6d5dc75803668a3861a37e16d4352844c
kylianLaw <iladelek@gmail.com>
2025-05-15 23:46:35 -0400

    modified the order php, made it fillable

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'name',
+        'email',
+        'address',
+        'payment_type',
+        'total_price',
+        'items',
+    ];
+
+    protected $casts = [
+        'items' => 'array',
+    ];
 }
